696b0e1d2d427c5cd6b198d45855c8ef291e2f04
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-04-22 00:24:14 +0100

    build /run

--- api/build.sh
+++ api/build.sh
@@ -1,3 +1,6 @@
+echo "-- Building API --"
+docker build -t bidyourauction .
+
 echo "-- Running API --"
 docker run -d -p 8080:8080 --name bidyourauction bidyourauction
 docker run -it bidyourauction python main.py

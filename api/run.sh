2dda77308e47ebbc3e35bd9b213f57818bc3efda
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-04-22 00:22:42 +0100

    sera que est´å?

--- api/run.sh
+++ api/run.sh
@@ -1,7 +1,7 @@
-api_image="bidyourauction:latest"
-api_container="bidyourauction"
+echo "-- Building API --"
+docker build -t bidyourauction .
 
 echo "-- Running API --"
-docker stop $api_container || true && docker rm $api_container || true
-docker run --name $api_container -p 8080:8080 $api_image
+docker run -d -p 8080:8080 --name bidyourauction bidyourauction
+docker run -it bidyourauction python main.py
 
--- postgres/run.sh
+++ postgres/run.sh
@@ -1,7 +1,5 @@
-bd_image="postgres:latest"
-bd_cointainer="postgres"
-
+echo "-- Building Database --"
+docker pull postgres
 echo "-- Running Database --"
-docker stop $bd_cointainer || true && docker rm $bd_cointainer || true
-docker run --name $bd_cointainer -p 5432:5432  $bd_image
+docker run -d -p 5432:5432 --name postgres -e POSTGRES_PASSWORD=postgres postgres
 

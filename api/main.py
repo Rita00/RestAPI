64bfd61321450d6a007a52eaf6522b6e9900af54
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-04-22 17:36:47 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -7,10 +7,10 @@ if __name__=='__main__':
         POSTGRES_USER=os.environ.get('POSTGRES_USER')
         POSTGRES_PASSWORD=os.environ.get('POSTGRES_PASSWORD')
         POSTGRES_DB=os.environ.get('POSTGRES_DB')
-        print("User: ",POSTGRES_USER)
-        print("Password: ***")
-        print("DB: ",POSTGRES_DB)
+        try:
+            db = database.Database(POSTGRES_USER,POSTGRES_PASSWORD,"postgres","5432",POSTGRES_DB)
+            db.print()
+        except:
+            print("Error connecting to the database")
     except:
         print("Error getting environement variables")
-    db = database.Database(POSTGRES_USER,POSTGRES_PASSWORD,"postgres","5432",POSTGRES_DB)
-    db.print()

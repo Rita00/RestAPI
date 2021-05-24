ac85846b656f0a6853803b9a779ef923caecac70
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-05-24 15:44:48 +0100

    Criar base de dados

--- docker-compose.sh
+++
@@ -1,7 +0,0 @@
-#Remove todos os contentores a correr
-docker rmi -f bd2021_server:latest
-docker rmi -f bd2021_database:latest
-docker rm -v -f postgres
-docker rm -v -f bidyourauction
-
-docker-compose -f docker-compose.yml up --build

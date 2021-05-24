528c62d3987e388320f842d34095a260500fe5f7
Rita00 <rita.lapao00@gmail.com>
2021-05-24 19:10:38 +0100

    rollback

--- api/main.py
+++ api/main.py
@@ -26,7 +26,7 @@ def signUp():
             f"person_username='{content['username']}'"
         )
     except Exception as e:
-        db.connection.commit()
+        db.connection.rollback()
         print(e)
         return jsonify({'erro': 401})
     print(f"Added user #{id}")
@@ -44,16 +44,17 @@ def signIn():
         return jsonify({'erro': 401})
     return jsonify({'authToken': authToken})
 
+
 @app.route('/dbproj/leilao', methods=['POST'])
 def createAuction():
     """Criar Leilão"""
     id = None
     try:
         content = request.json
-        #vai buscar o criador da eleição
-        token = jwt.decode("username") #TODO
-        person_id=db.select("person_id",'participant', f'person_username={token}')
-        #insere o leilão
+        # vai buscar o criador da eleição
+        token = jwt.decode("username")  # TODO
+        person_id = db.select("person_id", 'participant', f'person_username={token}')
+        # insere o leilão
         id = db.insert(
             'auction',
             'code, min_price, begin_date, end_date, participant_person_id',
@@ -61,9 +62,9 @@ def createAuction():
             'person_id',
             f"person_username='{content['username']}'"
         )
-        #conta as versoes existentes
-        version = db.select("count(*)",'textual_description', f'auction_id={id}')+1
-        #insere os dados textuais do leilão
+        # conta as versoes existentes
+        version = db.select("count(*)", 'textual_description', f'auction_id={id}') + 1
+        # insere os dados textuais do leilão
         db.insert(
             'textual_description',
             'version, title, description, alteration_date, auction_id',
@@ -75,6 +76,7 @@ def createAuction():
         return jsonify({'erro': 401})
     print(f"Added user #{id}")
     return jsonify({'leilãoId': id})
+
 
 @app.route('/dbproj/leiloes/<keyword>', methods=['GET'])
 def listCurrentAuctions(keyword):
@@ -94,7 +96,7 @@ def home():
 
 
 if __name__ == '__main__':
-    #localhost
+    # localhost
     """
     db_host = None
     if 'Windows' == platform.system():  # windows
@@ -116,22 +118,22 @@ if __name__ == '__main__':
     print(BIDYOURAUCTION_DB)
     #db = database.Database(BIDYOURAUCTION_USER, BIDYOURAUCTION_PASSWORD, db_host, "5432", BIDYOURAUCTION_DB)
     """
-    #Heroku
+    # Heroku
 
-    #BIDYOURAUCTION_USER = os.environ.get('BIDYOURAUCTION_USER')
-    #BIDYOURAUCTION_PASSWORD = os.environ.get('BIDYOURAUCTION_PASSWORD')
-    #BIDYOURAUCTION_HOST = os.environ.get('BIDYOURAUCTION_HOST')
-    #BIDYOURAUCTION_PORT = os.environ.get('BIDYOURAUCTION_PORT')
-    #BIDYOURAUCTION_DB = os.environ.get('BIDYOURAUCTION_DB')
-    #print(BIDYOURAUCTION_USER,BIDYOURAUCTION_PASSWORD,BIDYOURAUCTION_HOST,BIDYOURAUCTION_PORT,BIDYOURAUCTION_DB)
+    # BIDYOURAUCTION_USER = os.environ.get('BIDYOURAUCTION_USER')
+    # BIDYOURAUCTION_PASSWORD = os.environ.get('BIDYOURAUCTION_PASSWORD')
+    # BIDYOURAUCTION_HOST = os.environ.get('BIDYOURAUCTION_HOST')
+    # BIDYOURAUCTION_PORT = os.environ.get('BIDYOURAUCTION_PORT')
+    # BIDYOURAUCTION_DB = os.environ.get('BIDYOURAUCTION_DB')
+    # print(BIDYOURAUCTION_USER,BIDYOURAUCTION_PASSWORD,BIDYOURAUCTION_HOST,BIDYOURAUCTION_PORT,BIDYOURAUCTION_DB)
 
     # db = database.Database(BIDYOURAUCTION_USER, BIDYOURAUCTION_PASSWORD, db_host, "5432", BIDYOURAUCTION_DB)
     db = database.Database(
-        user = "vtxuzrplfviiht", 
-        password = "eb4ada6829ffce0e0f516062ea258ca6aa14d2fd85ea907ad910aa62eaf1412a", 
-        host = "ec2-34-254-69-72.eu-west-1.compute.amazonaws.com", 
-        port = "5432", 
-        database = "das7ket3c5aarn"
+        user="vtxuzrplfviiht",
+        password="eb4ada6829ffce0e0f516062ea258ca6aa14d2fd85ea907ad910aa62eaf1412a",
+        host="ec2-34-254-69-72.eu-west-1.compute.amazonaws.com",
+        port="5432",
+        database="das7ket3c5aarn"
     )
 
     # db = database.Database(user, user, db_host, "5432", 'bidyourauction_db')

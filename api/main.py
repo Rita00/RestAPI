64fe69496020bf2d3a6e6a96bb57f2e62f2de12d
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-05-24 19:18:19 +0100

    only with env variables

--- api/main.py
+++ api/main.py
@@ -44,16 +44,16 @@ def signIn():
         return jsonify({'erro': 401})
     return jsonify({'authToken': authToken})
 
+
 @app.route('/dbproj/leilao', methods=['POST'])
-def createAuction():
+def createAuction(username):
     """Criar Leilão"""
     id = None
     try:
         content = request.json
-        #vai buscar o criador da eleição
-        token = jwt.decode("username") #TODO
-        person_id=db.select("person_id",'participant', f'person_username={token}')
-        #insere o leilão
+        # vai buscar o criador da eleição
+        person_id = db.select("person_id", 'participant', f'person_username={username}')
+        # insere o leilão
         id = db.insert(
             'auction',
             'code, min_price, begin_date, end_date, participant_person_id',
@@ -61,9 +61,9 @@ def createAuction():
             'person_id',
             f"person_username='{content['username']}'"
         )
-        #conta as versoes existentes
-        version = db.select("count(*)",'textual_description', f'auction_id={id}')+1
-        #insere os dados textuais do leilão
+        # conta as versoes existentes
+        version = db.select("count(*)", 'textual_description', f'auction_id={id}') + 1
+        # insere os dados textuais do leilão
         db.insert(
             'textual_description',
             'version, title, description, alteration_date, auction_id',
@@ -76,6 +76,7 @@ def createAuction():
     print(f"Added user #{id}")
     return jsonify({'leilãoId': id})
 
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -83,43 +84,17 @@ def home():
 
 
 if __name__ == '__main__':
-    #localhost
-    """
-    db_host = None
-    if 'Windows' == platform.system():  # windows
-        db_host = '127.0.0.1'
-        user = 'postgres'
-    elif 'Darwin' == platform.system():  # macOS
-        db_host = 'docker.for.mac.localhost'
-        user = 'bidyourauction'
-    else:  # Linux (for docker)
-        db_host = 'postgres'
-        user = 'bidyourauction'
-
-    # os.environ['BIDYOURAUCTION_USER'] = 'postgres'
-    BIDYOURAUCTION_USER = os.getenv('BIDYOURAUCTION_USER')
-    print(BIDYOURAUCTION_USER)
+    BIDYOURAUCTION_USER = os.environ.get('BIDYOURAUCTION_USER')
     BIDYOURAUCTION_PASSWORD = os.environ.get('BIDYOURAUCTION_PASSWORD')
-    print(BIDYOURAUCTION_PASSWORD)
+    BIDYOURAUCTION_HOST = os.environ.get('BIDYOURAUCTION_HOST')
+    BIDYOURAUCTION_PORT = os.environ.get('BIDYOURAUCTION_PORT')
     BIDYOURAUCTION_DB = os.environ.get('BIDYOURAUCTION_DB')
-    print(BIDYOURAUCTION_DB)
-    #db = database.Database(BIDYOURAUCTION_USER, BIDYOURAUCTION_PASSWORD, db_host, "5432", BIDYOURAUCTION_DB)
-    """
-    #Heroku
-
-    #BIDYOURAUCTION_USER = os.environ.get('BIDYOURAUCTION_USER')
-    #BIDYOURAUCTION_PASSWORD = os.environ.get('BIDYOURAUCTION_PASSWORD')
-    #BIDYOURAUCTION_HOST = os.environ.get('BIDYOURAUCTION_HOST')
-    #BIDYOURAUCTION_PORT = os.environ.get('BIDYOURAUCTION_PORT')
-    #BIDYOURAUCTION_DB = os.environ.get('BIDYOURAUCTION_DB')
-    #print(BIDYOURAUCTION_USER,BIDYOURAUCTION_PASSWORD,BIDYOURAUCTION_HOST,BIDYOURAUCTION_PORT,BIDYOURAUCTION_DB)
+    print(BIDYOURAUCTION_USER, BIDYOURAUCTION_PASSWORD, BIDYOURAUCTION_HOST, BIDYOURAUCTION_PORT, BIDYOURAUCTION_DB)
     db = database.Database(
-        user = "vtxuzrplfviiht", 
-        password = "eb4ada6829ffce0e0f516062ea258ca6aa14d2fd85ea907ad910aa62eaf1412a", 
-        host = "ec2-34-254-69-72.eu-west-1.compute.amazonaws.com", 
-        port = "5432", 
-        database = "das7ket3c5aarn"
+        user=BIDYOURAUCTION_USER,
+        password=BIDYOURAUCTION_PASSWORD,
+        host=BIDYOURAUCTION_HOST,
+        port=BIDYOURAUCTION_PORT,
+        database=BIDYOURAUCTION_DB
     )
-
-    # db = database.Database(user, user, db_host, "5432", 'bidyourauction_db')
     app.run(debug=True, host='localhost', port=8080)
